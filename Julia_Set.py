24279f86632597f3c92230ab1c2f0a00c8e4eda1
yalcinkilic <yalcinkilic@sabanciuniv.edu>
2018-03-23 15:58:05 +0300

    Julia Set class is created

---
+++ Julia_Set.py
@@ -0,0 +1,48 @@
+"""
+This program is implementing Julia Set class which can plot the Julia sets of a given equation using matplotlib library of python
+For more information check documentation
+"""
+
+import matplotlib.pyplot as plt
+
+class Julia_Set(self):
+    def plot_Julia_Set(self):
+        """
+        This function plots the Julia set a particular class instance,self
+        """
+        pass
+    def set_constant(self,constant):
+        """
+        This function determines the c in the formula f(z) = z^2 + c
+        """
+        pass
+    def get_constant(self):
+        """
+        This function returns the value of constant
+        """
+        pass
+    def set_maxiter(self , max_iter):
+        """
+        This function sets max iteration
+        """
+        pass
+    def get_maxiter(self):
+        """
+        This function gets the value of max_iter
+        """
+        pass
+    def set_window(self , x_lower , x_upper , y_lower , y_upper):
+        """
+        This function sets the windows of the Julia set to be plotted
+        """
+
+    def get_window(self):
+        """
+        This functino get the coordinates of the window as a list
+        """
+        pass
+    def compute_n_th_iter(self , iter_num):
+        """
+        This function computes f(z) o f(z) o ... f(z) iter num times
+        """
+        pass
